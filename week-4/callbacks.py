9150cdf7ed3fe98bd4c92dcb1303337240ae2f49
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-11-15 13:36:02 -0500

    update numpy

--- week-4/callbacks.py
+++ week-4/callbacks.py
@@ -1,9 +1,14 @@
 import dash
+
 import dash_core_components as dcc
 import dash_html_components as html
+
 from dash.dependencies import Input, Output
 import pdb
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -14,6 +19,9 @@ app.layout = html.Div([
 
 # @app.route('/')
 # def render()
+
+# 7 month contract
+#
 
 
 @app.callback(
